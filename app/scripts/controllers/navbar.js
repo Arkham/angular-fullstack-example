1ba74b5b0f407726de020a789dd3dd3a72770056
Ju Liu <ju.liu@welaika.com>
2014-02-10 10:48:32 +0100

    added new header button

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -2,11 +2,17 @@
 
 angular.module('javascriptApp')
   .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
-    
+    $scope.menu = [
+      {
+        'title': 'Home',
+        'link': '/'
+      },
+      {
+        'title': 'Message',
+        'link': '/message'
+      }
+    ];
+
     $scope.isActive = function(route) {
       return route === $location.path();
     };
